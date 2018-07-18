94dadebedd31f31cd9370a95e50c12062f421a91
root <root@dnaserver.dnaserver>
2018-07-18 13:26:41 -0400

    Libreria basica para el Teclado 4x4

---
+++ p887micro.X/keypad.c
@@ -0,0 +1,85 @@
+ /*********************************************************
+ *  Libreria Basica para control de teclado matrix
+ *  Autor: Pablo Zarate email:pablo@digital-bo.com
+ *  Material de uso para aprendizaje en ELT436 U.E.B.
+ *  (nota) Las filas B7-B4 deben ser entradas pullups
+ *  Se debe definir los siguientes literales
+ *  #define KB_PORT Port de salida
+ *	#define KB_TRIS Tris de Port
+*******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2016 released beMicro.  All rights reserved.
+beMicro autoriza el uso, modificacion,copia y distribucion de este software.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES INCLUDING BUT NOT LIMITED TO ANY 
+INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR CONSEQUENTIAL DAMAGES, LOST PROFITS
+OR LOST DATA, COST OF PROCUREMENT OF SUBSTITUTE GOODS.
+*******************************************************************************/
+void KBSetup()
+{
+	KB_TRIS = 0xF0;
+	KB_PORT = 0x00;
+}
+char KBScan()
+{
+	char i = 4, val;
+	char row, col = 0b11110111; //Inicia Columna 1
+	while(i--)
+	{
+		KB_PORT = col;
+		NOP();
+		if((KB_PORT & 0xF0) != 0xF0)
+		{
+			row = (KB_PORT) & 0xF0; //Complemento
+			col = (col) & 0x0F;
+			while((KB_PORT & 0xF0) != 0xF0);
+			KB_PORT = 0x00;
+            return (row|col); //Retorna R1R2R3R4C1C2C3C4
+		}
+		col = col >> 1;		
+	}
+    KB_PORT = 0x00;
+	return 0;
+}
+char KBGetchar(char n)
+{
+	switch(n)
+	{
+		case 0b11101110: return 'D';
+		case 0b11101101: return '#';
+		case 0b11101011: return '0';
+		case 0b11100111: return '*';
+		case 0b11011110: return 'C';
+		case 0b11011101: return '9';
+		case 0b11011011: return '8';
+		case 0b11010111: return '7';
+		case 0b10111110: return 'B';
+		case 0b10111101: return '6';
+		case 0b10111011: return '5';
+		case 0b10110111: return '4';
+		case 0b01111110: return 'A';
+		case 0b01111101: return '3';
+		case 0b01111011: return '2';
+		case 0b01110111: return '1';
+		default: return '?';
+	}
+}
+
+void KBGetstring(char *msg, char len)
+{
+	char key;
+	while( len --)
+	{
+		do
+		{
+			key = KBScan();
+		} while(key == 0);
+		*msg = KBGetchar(key);
+		msg ++;
+        __delay_ms(100);
+	} 
+	*msg = 0;
+}
