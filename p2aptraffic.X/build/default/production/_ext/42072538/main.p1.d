39ed3124b53549bbf746b210a354fc7b746766f9
Pablo <pablinza@me.com>
2024-09-08 13:04:28 -0400

    Delete p2aptraffic.X directory

--- p2aptraffic.X/build/default/production/_ext/42072538/main.p1.d
+++
@@ -1,2 +0,0 @@
-build/default/production/_ext/42072538/main.p1:  \
-../p1apbase.X/main.c 
--- p2aptraffic.X/dist/default/production/__eeprom.d
+++
@@ -1,2 +0,0 @@
-dist/default/production/__eeprom.p1: \
-  /opt/microchip/xc8/v2.50/pic/sources/c99/pic/__eeprom.c
